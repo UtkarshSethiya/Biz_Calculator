baa2bca034089ffbfb9315433b02d7b87fb319f8
Utkarsh <sethiyautkarsh@gmail.com>
2022-02-12 12:18:34 +0530

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,23 @@
+
+import Header from "./header";
+import React from 'react';
+import Calculator from "./calculator";
+
+import './home.css';
+
+
+function Home(){
+
+    return(<div>
+     
+
+<Calculator/>
+
+
+<br></br><br></br><br></br>
+
+
+
+    </div>);
+}
+export default Home;
